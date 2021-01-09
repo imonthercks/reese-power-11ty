a5b730acc678846cc14d6cd914c80e3104942c85
C. J. Russell <imonthercks@gmail.com>
2021-01-09 12:53:18 -0600

    test logging of chance to win details

--- netlify_functions/chance_to_win.js
+++ netlify_functions/chance_to_win.js
@@ -1,5 +1,6 @@
 exports.handler = async function(event, context) {
   // your server-side functionality
+  console.log(JSON.stringify(event.body));
   return {
     statusCode: 200,
     body: JSON.stringify({message: "Hello World"})
