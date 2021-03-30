345db6ec55c2847ee181c0b4eb9dbc0e64e42f17
Aaron Schif <aschif@netdevgroup.com>
2021-03-29 22:11:10 -0500

    Don't show end date if it is null

--- src/_data/contentful_events.js
+++ src/_data/contentful_events.js
@@ -14,14 +14,18 @@ var renderFriendlySchedule = (startDateTime, endDateTime) => {
     var end = new Date(endDateTime);
 
     startText = dateformat(startDateTime, "fullDate") + "   " + dateformat(startDateTime, "shortTime");
-    var endText = "";
-    if (start.getDate() != end.getDate() || start.getMonth() != end.getMonth() || start.getFullYear() != end.getFullYear())
-        endText = endText + dateformat(endDateTime, "fullDate") + " ";
 
-    endText = endText + dateformat(endDateTime, "shortTime");
+    if (endDateTime) {
+        var endText = "";
+        if (start.getDate() != end.getDate() || start.getMonth() != end.getMonth() || start.getFullYear() != end.getFullYear()) {
+            endText = endText + dateformat(endDateTime, "fullDate") + " ";
+        }
 
-    return {start: startText, end: endText, full: startText + ' - ' + endText};
+        endText = endText + dateformat(endDateTime, "shortTime");
+        return {start: startText, end: endText, full: startText + ' - ' + endText};
+    }
 
+    return {start: startText, end: null, full: startText};
 }
 
 var renderDirectionUri = (address, location) => {
